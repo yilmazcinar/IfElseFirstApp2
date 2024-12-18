14e0fe67525ffc67dd1ff8f0dfc284ee5766f046
yilmazcinar <cinaryilmaz96@gmail.com>
2024-12-18 12:34:16 +0300

    Add files via upload

---
+++ IfElseFirstApp2/IfElseFirstApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Lütfen Bir Sayı Giriniz");
+
+        string number = Console.ReadLine();
+
+        int.TryParse(number, out int sayi);
+
+
+        if (sayi > 10)
+        {
+            Console.WriteLine("Sayı 10 dan büyüktür");
+        }
+        else
+        {
+            if (sayi < 10)
+            {
+                Console.WriteLine("Sayı 10 dan küçüktür");
+            }
+            else
+            {
+                Console.WriteLine(" Sayı 10 a eşittir ");
+            }
+        }
+        if (sayi % 2 == 0)
+        {
+            Console.WriteLine("Sayı çift sayıdır");
+        }
+        else
+        {
+            Console.WriteLine("Sayı tek Sayıdır");
+        }
+    }
+
+    
+}
